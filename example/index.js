53e336daea8775bbcd1b1a9dcebc342e91477e11
huhn <sehe89@gmail.com>
2019-08-30 23:33:11 +0200

    initial commit

---
+++ example/index.js
@@ -0,0 +1,20 @@
+var paymentModule = require('..')
+var app = require('express')()
+
+app.get("/", function (req, res) {
+    res.send('hello world from example!');
+});
+
+var options = {
+    mount: '/payment',
+    seed: "BZQISTHGKAEMDYGJXPPDIXHJJELETQYPOAWLMAKIUVSGQKCNKRUYFQPDKZCNMAXNAWEYWVXVWFFWPLMBD", // default: generateSeed()
+    // ...
+}
+
+let server = paymentModule(options, app)
+
+//app.use('/payments', )
+server.listen(3000, function () {
+    // Started Express app with payment paymentModule on '/payments'
+    console.log(`Server started on http://localhost:3000 `)
+})
