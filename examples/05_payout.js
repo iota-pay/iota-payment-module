9f536a3d54be7721ff984554aa4e12acafcb0750
Thoralf-M <thoralf.mue@gmail.com>
2019-09-12 14:07:37 +0200

    handle invalid payouts in database and update example

--- examples/05_payout.js
+++ examples/05_payout.js
@@ -2,7 +2,7 @@ var paymentModule = require('..')
 
 var address = "FDAMKPQTGRVIRWYIVDTAJSPBLKWA9OHLYPKBGMUFFMLAPQNAGBMUPOXC9KMLHAKRDMBTRBEE9ALBHQTVWJNAHYXBTC"
 
-paymentModule.payout(address, 1).then(result => {
+paymentModule.payout({address: address, value: 1}).then(result => {
     console.log("result", result)
 }, error => {
     console.log("error", error)
