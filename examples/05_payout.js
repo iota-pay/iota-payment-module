1bf18c7ce0c5a2c3b8f490257f597ef30e440398
Thoralf-M <thoralf.mue@gmail.com>
2019-09-15 17:39:28 +0200

    refactor payout stuff

--- examples/05_payout.js
+++ examples/05_payout.js
@@ -1,9 +1,11 @@
 var paymentModule = require('..')
 
-var address = "RRXNYBITPFVRVCTUYMKVE9GJSYOOQPUOAFODNLVJALNATYKMXSKFHXBMDTCIBJABIGVBESXIUNLXQECTYDWEPWCJPX"
+var address = "ht"
 
-paymentModule.payout.createPayout({address: address, value: 1}).then(result => {
+paymentModule.payout.send({address: address, value: 1}).then(result => {
     console.log("result", result)
 }, error => {
     console.log("error", error)
 })
+// .catch(e => {
+//   console.log(e)})
