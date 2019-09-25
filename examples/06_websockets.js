c172688b6ea5ba7a69fb9bac99ee8bb954b77004
Thoralf-M <thoralf.mue@gmail.com>
2019-09-25 22:22:22 +0200

    send info id dependent

--- examples/06_websockets.js
+++ examples/06_websockets.js
@@ -1,9 +1,25 @@
 var paymentModule = require('..')
-var app = require('express')()
+var express = require('express')
+var app = express()
 
 app.get("/", function (req, res) {
     res.sendFile(__dirname + '/06_websocket.html');
 });
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.post('/payment', async (req, res) => {
+  try {
+    const inputdata = req.body.data
+    let value = inputdata[0]
+    let data = inputdata[1]
+    //create payment
+    let payment = await paymentModule.payments.createPayment(value, data)
+    res.send(payment)
+  } catch (err) {
+    res.send(err)
+    console.log(err)
+  }
+})
 
 var options = {
     mount: '/payments',
