c67dbf5ee4542b4bc69e3444857e4babba962a65
huhn <sehe89@gmail.com>
2019-09-27 01:57:02 +0200

    fix js event handler and emit "paymentIncoming"

--- examples/03_events.js
+++ examples/03_events.js
@@ -1,11 +1,16 @@
 var paymentModule = require('..')
 var app = require('express')()
 
+var options = {
+    mount: '/',
+    value: 1
+    // ...
+}
 app.get("/", function (req, res) {
     res.send('hello world from 03_events example!');
 });
 
-let server = paymentModule.createServer(app)
+let server = paymentModule.createServer(app, options)
 
 // Start server with iota-payment module on '/'
 server.listen(3000, function () {
@@ -17,6 +22,11 @@ var onPaymentCreated = function (payment) {
     console.log('payment created!', payment);
 }
 
+//Create an event handler which is called, when a payment was created
+var onPaymentIncoming = function (payment) {
+    console.log('payment incoming!', payment);
+}
+
 
 //Create an event handler which is called, when a payment was successfull
 var onPaymentSuccess = function (payment) {
@@ -25,4 +35,5 @@ var onPaymentSuccess = function (payment) {
 
 //Assign the event handler to the events:
 paymentModule.on('paymentCreated', onPaymentCreated);
+paymentModule.on('paymentIncoming', onPaymentIncoming);
 paymentModule.on('paymentSuccess', onPaymentSuccess);
