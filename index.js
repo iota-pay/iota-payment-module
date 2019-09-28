c37f24abfb983f0154a2b7497536416b81428e63
Thoralf-M <thoralf.mue@gmail.com>
2019-09-28 21:16:56 +0200

    edit routes

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 module.exports = require('./lib/create-server')
-module.exports.payout = require('./lib/payout')
+module.exports.payout = require('./lib/payouts')
 module.exports.payments = require('./lib/payments')
+let { getTotalBalance } = require('./lib/Account')
+module.exports.getTotalBalance
