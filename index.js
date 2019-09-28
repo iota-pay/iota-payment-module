aff2608be1da83e666cd62dd339343ab50904117
Thoralf-M <thoralf.mue@gmail.com>
2019-09-28 21:29:08 +0200

    export getBalance

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 module.exports = require('./lib/create-server')
 module.exports.payout = require('./lib/payouts')
 module.exports.payments = require('./lib/payments')
-let { getTotalBalance } = require('./lib/Account')
-module.exports.getTotalBalance
+let { getBalance } = require('./lib/Account')
+module.exports.getBalance = getBalance
